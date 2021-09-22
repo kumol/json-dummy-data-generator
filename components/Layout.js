5741f323efa45734fc1fc6fafdd93c0af3741197
kumol <kumol01763@gmail.com>
2021-09-22 23:04:58 +0600

    structuring

---
+++ components/Layout.js
@@ -0,0 +1,9 @@
+const Layout = ({props, children})=>{
+    return(
+        <main>
+            {children}
+        </main>
+    )
+}
+
+export default Layout;
---
+++ pages/demo-api.js
@@ -0,0 +1,14 @@
+import Header from "../components/UI/Header";
+
+const DemoApi = ()=>{
+    return (
+        <div>
+            <Header></Header>
+            <div>
+                <p>Hello api</p>
+            </div>
+        </div>
+    )
+}
+
+export default  DemoApi
