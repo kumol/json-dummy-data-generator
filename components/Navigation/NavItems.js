74d3f5e18cd5c1654b0cf646f7e42c09f029d4f6
kumol <kumol01763@gmail.com>
2021-09-23 23:56:37 +0600

    undating designs of navigation

--- components/Navigation/NavItems.js
+++ components/Navigation/NavItems.js
@@ -2,9 +2,12 @@ import classes from "../../styles/NavItems.module.css";
 const NavItems = ({props, children})=>{
     return(
         <div className={classes.container}>
-            <nav>
-<p>Hello</p>
-            </nav>
+          <ul>
+            <li><a class="active" href="#home">Home</a></li>
+            <li><a href="#news">News</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li><a href="#about">About</a></li>
+          </ul>
         </div>
     )
 }
